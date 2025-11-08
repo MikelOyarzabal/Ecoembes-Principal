70bd0beba93e4a778b41a4298ec40aeebcb8b43c
Mikel Oyarzabal Riaño <mikel.oyarzabal@opendeusto.es>
2025-11-08 17:38:13 +0100

    creacion proyecto

---
+++ Ecoembes/src/test/java/DS_06/Ecoembes/EcoembesApplicationTests.java
@@ -0,0 +1,13 @@
+package DS_06.Ecoembes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoembesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
